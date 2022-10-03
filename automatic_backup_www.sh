16b0c41c27d5eda5c5a23756edef5be7fdcc843b
l3modz <l3modz@gmail.com>
2022-10-03 10:35:43 +0700

    Add files via upload

---
+++ automatic_backup_www.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright under GNU General Public License
+if [ `date +%Y%m%d` = "$day" ]; then
+    exit 0;
+fi
+
+tgl=date +"%m-%d-%y"
+NOW=$(date +"%m-%d-%Y")
+FILE="backup.$NOW.tar.gz"
+
+#tar -zcvf backup.www/backup.www.passkeeper25.$FILE /var/www/html/passkeeper25
+tar -zcvf backup.www/backup.www.YOUR_PROJECT.$FILE /var/www/html/YOUR_PROJECT
+
+#add to crontab, 
+# crontab -e
+# */30 * * * * ./backup.www.passkeeper.sh
